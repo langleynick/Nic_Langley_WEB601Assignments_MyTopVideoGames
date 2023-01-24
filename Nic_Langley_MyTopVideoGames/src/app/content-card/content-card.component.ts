8b667af81e59e8ebfbadc994a7e53e32d91dd046
langleynick <w0106967@myscc.ca>
2023-01-24 17:18:00 -0500

    Created array Content along with README
    TODO: Add array of content to content list and add it to HTML template to view.

--- Nic_Langley_MyTopVideoGames/src/app/content-card/content-card.component.ts
+++ Nic_Langley_MyTopVideoGames/src/app/content-card/content-card.component.ts
@@ -1,34 +1,36 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent{
-contentItem1:Content = {
+contentArray: Content[];
+constructor(){
+  this.contentArray = [{
   id: 1,
   title: "Jedi Fallen Order",
   description: "A young Jedi must go on a journey to find himself and rediscover the force.",
   creator: "EA Games",
   imgURL: "",
   type: "Sci-Fi"
-}
-contentItem2:Content = {
+},
+{
   id: 2,
   title: "God of War",
   description: "A seasoned warrior does what he must to protect his son.",
   creator: "Santa Monica Studio",
   imgURL: "",
   type: "Action"
-}
-contentItem3:Content = {
+},
+{
   id: 3,
   title: "The Last of Us",
   description: "In a post apocolyptic world, Joel must protect the only hope society has of survival",
   creator: "Naughty Dog",
   imgURL: "",
   type: "Action"
+}]
 }
 }
