6d6045c08e2bb5b55576968c992bfa9a4e7b7aa8
langleynick <w0106967@myscc.ca>
2023-03-09 15:24:44 -0500

    Moved content list
    Added contentDB.ts file and moved array of content from content-list component to the new file

--- Nic_Langley_MyTopVideoGames/src/app/content-list/content-list.component.ts
+++ Nic_Langley_MyTopVideoGames/src/app/content-list/content-list.component.ts
@@ -12,67 +12,6 @@ export class ContentListComponent implements OnInit{
     name = "Nick Langley";
     constructor() {
       this.title = "";
-      this.content = [
-        {
-          id: 1,
-          title: "The Last of Us",
-          description: "In a post apocolyptic world, Joel must protect the only hope society has of survival.",
-          creator: "Naughty Dog",
-          type: "Action",
-          tags: ["Horror", "Action", "Shooting"]
-        },
-        {
-          id: 2,
-          title: "God of War",
-          description: "A seasoned warrior does what he must to protect his son.",
-          creator: "Santa Monica Studio",
-          imgURL: "../assets/images/controllerIMG.png",
-          type: "Action",
-          tags: ["Open-World", "Fighting", "Mythology"]
-        },
-        {
-          id: 3,
-          title: "Jedi Fallen Order",
-          description: "A young Jedi must go on a journey to find himself and rediscover the force.",
-          creator: "EA Games",
-          imgURL: "../assets/images/controllerIMG.png",
-          tags: ["Science Fiction", "Action", "Star Wars"]
-        },
-        {
-          id: 4,
-          title: "Call of Duty: Modern Warfare 2",
-          description: "New revamp of an old First-Person Shooter.",
-          creator: "Infinity Ward",
-          imgURL: "../assets/images/controllerIMG.png",
-          type: "Shooting",
-          tags: ["Violence", "Sensitive Matters", "Shooting"]
-        },
-        {
-          id: 5,
-          title: "NBA 2k23",
-          description: "A basketball game where you can create your own career or play as real basketball teams.",
-          creator: "2k Games",
-          imgURL: "../assets/images/controllerIMG.png",
-          tags: ["Realism", "Basketball", "Celebrities"]
-        },
-        {
-          id: 6,
-          title: "Skyrim",
-          description: "An old-style classic Role Playing Game where you can choose your own path.",
-          creator: "Bethesda Studios",
-          imgURL: "../assets/images/controllerIMG.png",
-          type: "RPG",
-          tags: ["Ancient", "Fiction", "Decision-making"]
-        },
-        {
-          id: 7,
-          title: "Uncharted: Among Theives",
-          description: "A treasure hunter seeks fortune in an ancient city.",
-          creator: "Naughty Dog",
-          imgURL: "../assets/images/controllerIMG.png",
-          tags: ["Ancient", "Fiction", "Treasure"]
-        }
-      ]
     }
     ngOnInit(): void {
       
