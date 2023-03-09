c1499d61d4f841d9cc813aa55f375ee2bff597f0
langleynick <w0106967@myscc.ca>
2023-03-09 17:30:43 -0500

    Added messages

--- Nic_Langley_MyTopVideoGames/src/app/game-service.service.ts
+++ Nic_Langley_MyTopVideoGames/src/app/game-service.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 import { Content } from './helper-files/content-interface';
 import { CONTENT } from './helper-files/contentDB';
 
@@ -8,13 +9,16 @@ import { CONTENT } from './helper-files/contentDB';
 })
 export class GameServiceService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getGames (): Observable <Content[]> {
-    return of (CONTENT);
+    const games = of(CONTENT)
+    this.messageService.add('Content array loaded!');
+    return games;
   }
   getGame (id : number ): Observable<Content[]> {
-    let game: Content []= CONTENT.filter(obj => obj.id == id);
+    const game = CONTENT.filter(obj => obj.id == id);
+    this.messageService.add('Content ID: ' + id)
     return of (game);
   }
 }
